2cffc488eaaae50c7d0b7f8aa2e58ca8c0a082d1
mgushee <matt@gushee.net>
2012-11-03 15:22:46 -0600

    Began setup API.

--- src/sappari-setup.scm
+++ src/sappari-setup.scm
@@ -5,8 +5,9 @@
 ;;;   See the LICENSE file for details.
 
 (module sappari-setup 
-        (setup-site-skeleton
-         install-package)
+        *
+;        (setup-site-skeleton
+;         install-package)
 
         (import scheme)
         (import chicken)
@@ -19,12 +20,37 @@
         
 
 ;;; [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
+;;; ------------------------------------------------------------------------
+
+(define +default-site-skeleton+
+  '("build" "content" "etc" "extensions" ("sites" ("default")) "schemas"
+          ("static" ("fonts" "pages" "scripts" "style" "media"))
+          ("themes" (("plain" ("pink" "green"))))))
+
+
+;;; ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+
+
+
+;;; [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[
 ;;; --  PUBLIC API  --------------------------------------------------------
 
 (define (setup-site-skeleton #!key (parent-path ".") (site-dirname "sappari"))
   (let ((site-root (make-pathname parent-path site-dirname)))
-    (create-directory site-root)))
-  ;; Not done by a long shot
+    (create-directory site-root)
+    (change-directory site-root)
+    (let loop ((dirs +default-site-skeleton+))
+      (if (null? dirs)
+        #t
+        (let ((head (car dirs)))
+          (if (string? head)
+            (create-directory head)
+            (let ((sub (car head)))
+              (create-directory sub)
+              (change-directory sub)
+              (loop (cdr head))
+              (change-directory "..")))
+          (loop (cdr dirs)))))))
 
 
 ;;; ]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
